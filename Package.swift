04882098d3b57171fa47c69c320d68bd7bc383f5
marcprux <mwp1@cornell.edu>
2024-07-05 15:51:39 -0400

    Disable failing tests on Android emulator

--- Package.swift
+++ Package.swift
@@ -6,12 +6,12 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
-        .library(name: "SkipScript", targets: ["SkipScript"]),
+        .library(name: "SkipScript", type: .dynamic, targets: ["SkipScript"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.8.46"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.6.11"),
-        .package(url: "https://source.skip.tools/skip-ffi.git", from: "0.3.2"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.9.4"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.7.0"),
+        .package(url: "https://source.skip.tools/skip-ffi.git", from: "0.3.3"),
     ],
     targets: [
         .target(name: "SkipScript", dependencies: [
